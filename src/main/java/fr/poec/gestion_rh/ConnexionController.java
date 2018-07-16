a1884b36678f3dcc77c5b5bca8552d6a5d53f182
SupSonic <clementbrieuc@gmail.com>
2018-07-16 16:48:46 +0200

    modif connexion fxml

---
+++ src/main/java/fr/poec/gestion_rh/ConnexionController.java
@@ -0,0 +1,13 @@
+package fr.poec.gestion_rh;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+public class ConnexionController {
+
+	@FXML
+	private void { 
+	Parent home_page_parent = FXMLLoader.load(getClass().getResource("proto_projet_connexion.fxml"));
+	}
+}
