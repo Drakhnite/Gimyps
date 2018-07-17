21d285dad4637d72c94c4e18fec9ca36de0dc3fb
SupSonic <clementbrieuc@gmail.com>
2018-07-17 11:29:24 +0200

    code java accueil

--- src/main/java/fr/poec/gestion_rh/Main.java
+++ src/main/java/fr/poec/gestion_rh/Main.java
@@ -1,15 +1,16 @@
 package fr.poec.gestion_rh;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 public class Main extends Application{
 		@Override
 		public void start(Stage primaryStage) {
 			try {
-				BorderPane root = new BorderPane();
+				Parent root = FXMLLoader.load(getClass().getResource("/gestion_rh/proto_projet_connexion.fxml"));
 				Scene scene = new Scene(root,400,400);
 				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 				primaryStage.setScene(scene);
