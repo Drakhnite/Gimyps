13183da28688959b712290dffe14b237e31108d1
SupSonic <clementbrieuc@gmail.com>
2018-07-17 12:30:26 +0200

    controller connexion java

--- src/main/java/fr/poec/gestion_rh/MainController.java
+++ src/main/java/fr/poec/gestion_rh/MainController.java
@@ -20,7 +20,7 @@ public class MainController {
 	@FXML
 	private PasswordField txtPassword;
 	
-	public void Login(ActionEvent event) throws Exception {
+	public void Seconnecter(ActionEvent event) throws Exception {
 		if(txtUserName.getText().equals("user") && txtPassword.getText().equals("password")) {
 			txtConnexion.setText("Connexion réussie");
 			Stage primarystage = new Stage();
