988289e926f12d5749a4962429aa0ca747d64e11
Richard LEMEE <0671472668@orange.fr>
2018-07-19 15:13:36 +0200

    Page d'accueil UP

--- src/main/java/fr/poec/gestion_rh/Main.java
+++ src/main/java/fr/poec/gestion_rh/Main.java
@@ -1,38 +1,46 @@
-package fr.poec.gestion_rh;
-
-import java.io.IOException;
-
-import fr.poec.gestion_rh.table.Login;
-import fr.poec.gestion_rh.table.controler.Controler_Login;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/gestion_rh/proto_projet_connexion.fxml"));
-			Scene scene = new Scene(root, 400, 400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args) throws IOException {
-
-		Controler_Login loginC = new Controler_Login();
-
-		System.out.println("coucou");
-		for (Login login : loginC.getDataFromTable()) {
-			System.out.println(Login.getUser());
-
-		}
-		launch(args);
-	}
-}
+//package fr.poec.gestion_rh;
+//
+//import java.io.IOException;
+//
+//import fr.poec.gestion_rh.table.Login;
+//import fr.poec.gestion_rh.table.controler.Controler_Login;
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.stage.Stage;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//
+//public class Main extends Application {
+//	@Override
+//	public void start(Stage primaryStage) {
+//		
+//		this.primaryStage = primaryStage;
+//        this.primaryStage.setTitle("AddressApp");
+//
+//        init();
+//
+//        showPersonOverview();
+//    }
+//		try {
+//			Parent root = FXMLLoader.load(getClass().getResource("Root.fxml"));
+//			Scene scene = new Scene(root, 400, 400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	public static void main(String[] args) throws IOException {
+//
+//		Controler_Login loginC = new Controler_Login();
+//
+//		System.out.println("coucou");
+//		for (Login login : loginC.getDataFromTable()) {
+//			System.out.println(Login.getUser());
+//
+//		}
+//		launch(args);
+//	}
+//}
---
+++ src/main/java/fr/poec/gestion_rh/MainApp.java
@@ -0,0 +1,74 @@
+package fr.poec.gestion_rh;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane root;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("GIMYPS");
+
+        initRootLayout();
+
+        showConnexion();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("\\fxml\\root.fxml"));
+            root = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the person overview inside the root layout.
+     */
+    public void showConnexion() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("\\fxml\\proto_projet_connexion.fxml"));
+            AnchorPane connexion = (AnchorPane) loader.load();
+
+            // Set connexion into the center of root layout.
+            root.setCenter(connexion);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
