b6ce15f08cc79395af9681437d7141d58199332b
SupSonic <clementbrieuc@gmail.com>
2018-07-20 11:03:05 +0200

    ajout view controllers

--- src/main/java/fr/poec/gestion_rh/controllers/PersonnelController.java
+++ src/main/java/fr/poec/gestion_rh/controllers/PersonnelController.java
@@ -62,6 +62,24 @@ public void Accueil (ActionEvent event) throws Exception {
 	primarystage.show();
 }
 
+public void CotisationsPatro (ActionEvent event) throws Exception {
+	Stage primarystage = new Stage();
+	Parent root = FXMLLoader.load(getClass().getResource("/gestion_rh/proto_projet_cotisationsPatro.fxml"));
+	Scene scene = new Scene(root,400,400);
+	scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	primarystage.setScene(scene);
+	primarystage.show();
+}
+
+public void CotisationsSal (ActionEvent event) throws Exception {
+	Stage primarystage = new Stage();
+	Parent root = FXMLLoader.load(getClass().getResource("/gestion_rh/proto_projet_cotisationsSal.fxml"));
+	Scene scene = new Scene(root,400,400);
+	scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	primarystage.setScene(scene);
+	primarystage.show();
+}
+
 // Fonction de déconnexion à modifier
 public void Deconnexion (ActionEvent event) throws Exception {
 	Stage primarystage = new Stage();
