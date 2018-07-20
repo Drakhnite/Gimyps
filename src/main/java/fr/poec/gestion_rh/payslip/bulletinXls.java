45d6464062b10a07b8a45457c9dae74266010213
Richard LEMEE <0671472668@orange.fr>
2018-07-20 14:55:07 +0200

    do miracle

--- src/main/java/fr/poec/gestion_rh/payslip/bulletinXls.java
+++ src/main/java/fr/poec/gestion_rh/payslip/bulletinXls.java
@@ -8,28 +8,29 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import jxl.Sheet;
-import jxl.Workbook;
-import jxl.read.biff.BiffException;
+//import jxl.Sheet;
+//import jxl.Workbook;
+//import jxl.read.biff.BiffException;
 
 public class bulletinXls {
 
-	public Map<Integer, List<String>> readJExcel(String fileLocation) throws IOException, BiffException {
+	public Map<Integer, List<String>> readJExcel(String fileLocation) throws IOException/*, BiffException */{
 
-		Map<Integer, List<String>> data = new HashMap<>();
-
-		Workbook workbook = Workbook.getWorkbook(new File(fileLocation));
-		Sheet sheet = workbook.getSheet(0);
-		int rows = sheet.getRows();
-		int columns = sheet.getColumns();
-
-		for (int i = 0; i < rows; i++) {
-			data.put(i, new ArrayList<String>());
-			for (int j = 0; j < columns; j++) {
-				data.get(i).add(sheet.getCell(j, i).getContents());
-			}
-		}
-		return data;
+		return null;
+//		Map<Integer, List<String>> data = new HashMap<>();
+//
+//		Workbook workbook = Workbook.getWorkbook(new File(fileLocation));
+//		Sheet sheet = workbook.getSheet(0);
+//		int rows = sheet.getRows();
+//		int columns = sheet.getColumns();
+//
+//		for (int i = 0; i < rows; i++) {
+//			data.put(i, new ArrayList<String>());
+//			for (int j = 0; j < columns; j++) {
+//				data.get(i).add(sheet.getCell(j, i).getContents());
+//			}
+//		}
+//		return data;
 	}
 //	FileOutputStream fileOut = new FileOutputStream("temp.xls");
 //	workbook.write();
