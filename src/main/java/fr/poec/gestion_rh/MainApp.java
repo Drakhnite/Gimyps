d32cd3f28361a765e91cd024a8f5ca5939d3a39d
Richard LEMEE <0671472668@orange.fr>
2018-07-20 14:59:44 +0200

    test

--- src/main/java/fr/poec/gestion_rh/MainApp.java
+++ src/main/java/fr/poec/gestion_rh/MainApp.java
@@ -11,64 +11,65 @@ import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
-    private Stage primaryStage;
-    private BorderPane root;
+	private Stage primaryStage;
+	private BorderPane root;
 
-    @Override
-    public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("GIMYPS");
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("GIMYPS");
 
-        initRootLayout();
+		initRootLayout();
 
-        showConnexion();
-    }
+		showConnexion();
+	}
 
-    /**
-     * Initializes the root layout.
-     */
-    public void initRootLayout() {
-        try {
-            // Load root layout from fxml file.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("\\fxml\\root.fxml"));
-            root = (BorderPane) loader.load();
+	/**
+	 * Initializes the root layout.
+	 */
+	public void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("\\fxml\\root.fxml"));
+			root = (BorderPane) loader.load();
 
-            // Show the scene containing the root layout.
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * Shows the person overview inside the root layout.
-     */
-    public void showConnexion() {
-        try {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("\\fxml\\proto_projet_connexion.fxml"));
-            AnchorPane connexion = (AnchorPane) loader.load();
+	/**
+	 * Shows the person overview inside the root layout.
+	 */
+	public void showConnexion() {
+		try {
+			// Load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("\\fxml\\proto_projet_connexion.fxml"));
+			AnchorPane connexion = (AnchorPane) loader.load();
 
-            // Set connexion into the center of root layout.
-            root.setCenter(connexion);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			// Set connexion into the center of root layout.
+			root.setCenter(connexion);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * Returns the main stage.
-     * @return
-     */
-    public Stage getPrimaryStage() {
-        return primaryStage;
-    }
+	/**
+	 * Returns the main stage.
+	 * 
+	 * @return
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
